b5f2febf7e4d0a42b49aaff832c0e28830323fd7
opsuperchain <opsuperchain@proton.me>
2025-02-18 13:35:08 -0600

    Convert superchain-async to submodule

--- superchain-async/src/interface/async/RemoteExampleAsyncEnabled.sol
+++
@@ -1,7 +0,0 @@
-
-interface myAsyncFunction1Promise {
-    function then(function(uint256) external) external;
-}
-interface RemoteExampleAsyncEnabled {
-    function myAsyncFunction1() external returns (myAsyncFunction1Promise);
-}
--- superchain-async/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    globalSetup: './test/integration/setup.ts',
-    setupFiles: []  // Remove setupFiles since we're using globalSetup
-  }
-}) 
